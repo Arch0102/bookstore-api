62fa78588430f5eca080693338fd67d8121514fd
Santiago Ortiz <151885267+Arch0102@users.noreply.github.com>
2026-04-17 21:39:08 -0500

    feat: add auth and user DTOs for request and response

---
+++ src/main/java/com/taller/bookstore/dto/request/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.taller.bookstore.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+    @NotBlank(message = "name: must not be blank")
+    private String name;
+
+    @NotBlank(message = "email: must not be blank")
+    @Email(message = "email: must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "password: must not be blank")
+    @Size(min = 8, message = "password: must have at least 8 characters")
+    private String password;
+}
